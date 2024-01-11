ca258e051836ffe6e1d915b607627e2801f442a9
VlaDislav <92807808+kng111@users.noreply.github.com>
2024-01-12 00:08:08 +0300

    Update samolet.py

--- newserv/samolet.py
+++ newserv/samolet.py
@@ -1,28 +1,18 @@
 from datetime import datetime
-
 def check_flight_status(schedule_time, actual_time):
-
     schedule_time = datetime.strptime(schedule_time, "%H:%M")
     actual_time = datetime.strptime(actual_time, "%H:%M")
-
-    # Рассчитываем разницу во времени
     time_difference = actual_time - schedule_time
-
-    # Определяем статус прилета
     if time_difference.total_seconds() > 0:
         return f"Самолет опаздывает. Задержка: {time_difference}"
     elif time_difference.total_seconds() < 0:
         return f"Самолет прилетел раньше. Опережение: {-time_difference}"
     else:
         return "Самолет прилетел вовремя."
-
-
 schedule_time_input = input("Введите время прибытия по расписанию (в формате ЧЧ:ММ): ")
 actual_time_input = input("Введите фактическое время прибытия (в формате ЧЧ:ММ): ")
-
 try:
     result = check_flight_status(schedule_time_input, actual_time_input)
-
     print(result)
 except Exception as e:
     print(e)
